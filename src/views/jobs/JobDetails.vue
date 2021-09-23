954941fb9671b624d2be528116825906af5d9304
murilodpl <inayievmpl@gmail.com>
2021-09-23 17:48:28 -0300

    Update Jobs and Job page - Add fetch

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,17 +1,29 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ job.id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["id"],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
-};
+    props: ['id'],
+    data() {
+        return {
+            job: null,
+        }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then((res) => res.json())
+            .then((data) => (this.job = data))
+            .catch((err) => console.log(err.message))
+    },
+}
 </script>
 
 <style>
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,14 @@
 <template>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-            <h2>{{ job.title }}</h2>
-        </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
+    </div>
+    <div v-else>
+        <p>Loading jobs...</p>
     </div>
 </template>
 
@@ -11,12 +16,14 @@
 export default {
     data() {
         return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-                { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-            ],
+            jobs: [],
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+            .then((res) => res.json())
+            .then((data) => (this.jobs = data))
+            .catch((err) => console.log(err.message))
     },
 }
 </script>
